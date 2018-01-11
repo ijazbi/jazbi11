0cdce9f492a5068390e40aa8378121b6711a3929
Sim Brar <sim.brar@hootsuite.com>
2018-01-11 11:02:08 -0800

    remove twitter creds

--- index.js
+++ index.js
@@ -22,17 +22,24 @@ var secret = '';
 // Client for hydrating twitter data - ADD YOUR OWN CREDENTIALS
 // Otherwise, app will not show up in plugin list
 
-try {
-  var twitterClient = new Twitter({
-    consumer_key: '',
-    consumer_secret: '',
-    access_token: '',
-    access_token_secret: ''
-  });
-} catch(error) {
+/*
+try { */
+
+var twitterClient = new Twitter({
+  consumer_key: '',
+  consumer_secret: '',
+  access_token: '',
+  access_token_secret: ''
+});
+
+
+//} catch(error) {
+if (!twitterClient) {
   console.log('No Twitter API credenitals found');
-  process.exit(1);
 }
+  //process.exit(1);
+//}
+
 
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
